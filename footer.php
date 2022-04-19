4f82d5ca6aa55359dc21911c4f8536f5f2ba697e
afnnnnn <103879224+afnnnnn@users.noreply.github.com>
2022-04-19 15:08:07 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,4 @@
 
+echo '<a href="https://vt.tiktok.com/ZSek7txUF//html/">TikTok</a>';
+echo '<a href="https://www.facebook.com/Dr.BROWNIESKuantan/html/">Facebook</a>';
+echo '<a href="https://instagram.com/drbrownies_hq?igshid=5c8p64pi6g1t/html/">Instagram</a>';

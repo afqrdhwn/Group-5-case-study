7fbcd35af2dab7dd0614c137976a2476c94710aa
afqrdhwn <103871912+afqrdhwn@users.noreply.github.com>
2022-04-20 20:11:36 +0800

    Update summary.php

--- summary.php
+++ summary.php
@@ -1,42 +1,42 @@
+
+
 <table class ="summary" align="right">
 
   <div class="cart-subtotal">
   
     <thead >
         <tr >
-            <th colspan="3">Order Summary<br><hr></th>
+            <?php echo "<th colspan='3'>Order Summary<br><hr></th>"; ?>
         </tr>
     </thead>
     <tbody>
-	<tr>
-	<!-- subtotal -->
-        <td>Subtotal</td>
+	<?php echo "<tr>
+    <td>Subtotal</td>
 	<td></td>
-	<td class="subtotal-price" align="right">RM 0.00</td>
-	</tr>
+	<td class='subtotal-price' align='right'>RM 0.00</td>
+	</tr>";  ?>
 	
 	<tr>
 	<!-- tax -->
-        <td>6% GST</td>
+    <?php echo "<td>6% GST</td>
 	<td></td>
-	<td class="gst-price" align="right">RM 0.00</td>
+	<td class='gst-price' align='right'>RM 0.00</td>"; ?>
 	</tr>
 	
 	<tr>
-	<td colspan="3"><hr></td>
+	<?php echo "<td colspan='3'><hr></td>"; ?>
 	</tr>
 
 	<tr>
-	<!-- total -->
-        <td>Total</td>
+    <?php echo "<td>Total</td>
 	<td></td>
-	<td class="total-price" align="right">RM 0.00</td>
+	<td class='total-price' align='right'>RM 0.00</td>"; ?>
 	</tr>
 	
 	 </div>
 
 	<tr>
-        <td colspan="3"><br><button class="cart-button" onclick="checkout()">Checkout</button><br><br></td>
+     <?php echo "<td colspan='3'><br><button class='cart-button' onclick= 'return checkout();'>Checkout</button><br><br></td>"; ?>
 	</tr>
 	
 	</tbody>
@@ -45,7 +45,7 @@
 <table class ="cart">
     <thead>
         <tr >
-        <th colspan="5">My Shopping Cart<br><hr></th>
+        <?php echo "<th colspan='5'>My Shopping Cart<br><hr></th>"; ?>
         </tr>
     </thead>
     <tbody>
@@ -56,3 +56,11 @@
 	</tr>
 </tbody>
 </table>
+
+<script type="text/javascript">
+    function checkout(event){
+        alert("Your order has been submitted. Thank you");
+		window.location.href="index.php"; 
+        return true;
+    }
+</script>

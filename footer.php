244f6b98be7ba936e2b105a8d68cc01572d21746
afqrdhwn <103871912+afqrdhwn@users.noreply.github.com>
2022-04-20 16:31:16 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,19 +2,19 @@
 <div id="detail">
 <?php 
 echo "<h3>Contact Information</h3>
-        <p><i style='font-size:18px' class='material-icons'>home</i> No.15, Lorong IM12/3, Taman Astana Permai,<br>
+        <p><i style='font-size:24px' class='material-icons'>home</i> No.15, Lorong IM12/3, Taman Astana Permai,<br>
         &nbsp &nbsp &nbsp 25200 Kuantan,Pahang.</p>
-        <p><i style='font-size:18px' class='material-icons'>call</i> +6014 323 9792</p>
-        <p><a href='mailto:https://drbrownieshq@gmail.com/html/'><i style='font-size:18px' class='material-icons'>email</i> drbrownieshq@gmail.com</a></p>";				
+        <p><i style='font-size:24px' class='material-icons'>call</i> +6014 323 9792</p>
+        <p><a href='mailto:https://drbrownieshq@gmail.com/html/'><i style='font-size:24px' class='material-icons'>email</i> drbrownieshq@gmail.com</a></p>";				
 ?>
 </div>
 
 <div id="social">
 <?php 
 echo "<h3>Social Media</h3>";
-echo "<a href='https://vt.tiktok.com/ZSek7txUF//html/'><i style='font-size:18px' class='material-icons'>tiktok</i> @dr.brownieshq</a>";
-echo "<a href='https://instagram.com/drbrownies_hq?igshid=5c8p64pi6g1t/html/'><i style='font-size:18px' class='material-icons'>photo_camera</i> @drbrownies_hq</a>";
-echo "<a href='https://www.facebook.com/Dr.BROWNIESKuantan/html/'><i style='font-size:18px; color : white' class='material-icons'>facebook</i> Dr.BROWNIESKuantan</a><br>";
+echo "<a href='https://vt.tiktok.com/ZSek7txUF//html/'><i style='font-size:24px' class='material-icons'>tiktok</i> @dr.brownieshq</a>";
+echo "<a href='https://instagram.com/drbrownies_hq?igshid=5c8p64pi6g1t/html/'><i style='font-size:24px' class='material-icons'>photo_camera</i> @drbrownies_hq</a>";
+echo "<a href='https://www.facebook.com/Dr.BROWNIESKuantan/html/'><i style='font-size:24px; color : white' class='material-icons'>facebook</i> Dr.BROWNIESKuantan</a>";
+echo "<a href='http://shopee.com.my/dr.brownies_hq/html/'><i style='font-size:24px; color : white' class='material-icons'>local_mall</i> DrBrownies_HQ</a><br>";
 ?>
 </div>
-

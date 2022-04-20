c9841b642f4fb7eed9ff22fcd8058c06a3f5deb4
afqrdhwn <103871912+afqrdhwn@users.noreply.github.com>
2022-04-21 01:01:53 +0800

    Update topnav.php

--- topnav.php
+++ topnav.php
@@ -8,5 +8,5 @@ echo "<li><a href='http://www.wasap.my/60123456789/'><i class='material-icons'>p
 <div class ="logo">
 <?php echo "<li><img src='images/logo.png' width='135' height='auto'></li>"; ?>
 </div>
-<?php echo "<li><a href='#cart'><i class='material-icons'>shopping_cart</i></a></li>"; ?>
+<?php echo "<li><a href='menu.php#cart'><i class='material-icons'>shopping_cart</i></a></li>"; ?>
 </ul>

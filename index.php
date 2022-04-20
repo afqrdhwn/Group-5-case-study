593d86a23508cfb696b143783bffcd46f8035760
afqrdhwn <103871912+afqrdhwn@users.noreply.github.com>
2022-04-21 00:59:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html >
 <head>
-    	<title> Dr Brownies </title>
+    <title> Dr Brownies </title>
 	<?php include('./header.php') ?>
 </head>
 
@@ -17,17 +17,10 @@
 echo "<span style= 'font-size:300%;font-family:Roboto Serif;' >Your </span><br>";
 echo "<span style= 'font-family:Bebas Neue;font-size:800%;color:#ffffff' >Brownie's</span><br>";
 echo "<span style= 'font-size:300%;font-family:Roboto Serif;' >Expert</span><br>";
-echo "<button class='shop-button' onclick='window.location.href = '#menu';'> SHOP NOW </button>"; 
+echo "<button class=\"shop-button\" onclick=\"window.location.href = 'menu.php';\"> SHOP NOW </button>"; 
 ?>
 </section>
 
-<section id="?">
-
-</section>
-
-<section id="cart">			<!-- Afiq Ridhwan (2014161) -->
-<?php include('./summary.php') ?>
-</section>
 
 <section id="footer">				<!-- Afnan (1920311) -->
 <?php include('./footer.php') ?>

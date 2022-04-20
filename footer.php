ef9ad84a4fde266a273d33fff4ad0c46b5fc9b94
afqrdhwn <103871912+afqrdhwn@users.noreply.github.com>
2022-04-20 16:16:58 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,20 @@
-<ul>
-<div class ="footer">
-<?php
-echo "Follow us on";
-  echo "<li> <a href='https://vt.tiktok.com/ZSek7txUF//html/'>TikTok</a> </li>";
-  echo "<li> <a href='https://www.facebook.com/Dr.BROWNIESKuantan/html/'>Facebook</a> </li>";
-  echo "<li> <a href='https://instagram.com/drbrownies_hq?igshid=5c8p64pi6g1t/html/'>Instagram</a> </li>";
+
+<div id="detail">
+<?php 
+echo "<h3>Contact Information</h3>
+        <p><i style='font-size:18px' class='material-icons'>home</i> No.15, Lorong IM12/3, Taman Astana Permai,<br>
+        &nbsp &nbsp &nbsp 25200 Kuantan,Pahang.</p>
+        <p><i style='font-size:18px' class='material-icons'>call</i> +6014 323 9792</p>
+        <p><a href='mailto:https://drbrownieshq@gmail.com/html/'><i style='font-size:18px' class='material-icons'>email</i> drbrownieshq@gmail.com</a></p>";				
 ?>
 </div>
-</ul>
+
+<div id="social">
+<?php 
+echo "<h3>Social Media</h3>";
+echo "<a href='https://vt.tiktok.com/ZSek7txUF//html/'><i style='font-size:18px' class='material-icons'>tiktok</i> @dr.brownieshq</a>";
+echo "<a href='https://instagram.com/drbrownies_hq?igshid=5c8p64pi6g1t/html/'><i style='font-size:18px' class='material-icons'>photo_camera</i> @drbrownies_hq</a>";
+echo "<a href='https://www.facebook.com/Dr.BROWNIESKuantan/html/'><i style='font-size:18px; color : white' class='material-icons'>facebook</i> Dr.BROWNIESKuantan</a><br>";
+?>
+</div>
+

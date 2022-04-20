d21e456d62e567983c494edcd417abe7267d97cd
afnnnnn <103879224+afnnnnn@users.noreply.github.com>
2022-04-20 14:22:34 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
+<ul>
 <div class ="footer">
 <?php
-echo "Follow Us";
-  <ul>
-  echo '<li> <a href="https://vt.tiktok.com/ZSek7txUF//html/">TikTok</a> </li>';
-  echo '<li> <a href="https://www.facebook.com/Dr.BROWNIESKuantan/html/">Facebook</a> </li>';
-  echo '<li> <a href="https://instagram.com/drbrownies_hq?igshid=5c8p64pi6g1t/html/">Instagram</a> </li>';
-  </ul>
+echo "Follow us on";
+  echo "<li> <a href='https://vt.tiktok.com/ZSek7txUF//html/'>TikTok</a> </li>";
+  echo "<li> <a href='https://www.facebook.com/Dr.BROWNIESKuantan/html/'>Facebook</a> </li>";
+  echo "<li> <a href='https://instagram.com/drbrownies_hq?igshid=5c8p64pi6g1t/html/'>Instagram</a> </li>";
 ?>
 </div>
+</ul>

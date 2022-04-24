13108681b10c961556bab918a0e1463039340453
afqrdhwn <103871912+afqrdhwn@users.noreply.github.com>
2022-04-24 13:08:52 +0800

    Update topnav.php

--- topnav.php
+++ topnav.php
@@ -1,4 +1,6 @@
-<ul>      <!-- Afiq Ridhwan (2014161) -->
+<!-- Afiq Ridhwan (2014161) -->
+
+<ul>
 <div class ="call">
 <?php 
 echo "<li class = 'phoneno' ><br>+6012-345-6789<br><span class = 'time'> &nbsp 10:00am-10:00pm</span></li>";
